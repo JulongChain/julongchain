11164460a945756d6e4ca75eb35f1bce869af465
snl <snl@dxct.org>
2018-03-05 16:27:34 +0800

    [BJC-42][BJC-44][BJC-45][BJC-46] 添加系统智能合约桩的类

---
+++ src/main/java/org/bcia/javachain/core/ssc/CSSC.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright Dingxuan. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bcia.javachain.core.ssc;
+
+import org.bcia.javachain.core.smartcontract.shim.impl.Response;
+import org.bcia.javachain.core.smartcontract.shim.intfs.ISmartContractStub;
+
+/**
+ * 配置系统智能合约　Configure System Smart Contract,CSSC
+ *
+ * @author sunianle
+ * @date 3/5/18
+ * @company Dingxuan
+ */
+public class CSSC extends SystemSmartContractBase {
+
+    @Override
+    public Response init(ISmartContractStub stub) {
+        return null;
+    }
+
+    @Override
+    public Response invoke(ISmartContractStub stub) {
+        return null;
+    }
+
+    @Override
+    public String getSmartContractDescription() {
+        return null;
+    }
+}
