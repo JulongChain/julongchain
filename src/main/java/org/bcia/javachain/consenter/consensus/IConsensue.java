72c268c0911544ac531c4bd35635cd5ef70c5534
zhangmingyang <zmy@dxct.org>
2018-03-07 19:58:49 +0800

    [BJC-64] 1.修改Iconsensue的部分接口 2.新增msp的接口定义 3.修改localmsp的部分接口

--- src/main/java/org/bcia/javachain/consenter/consensus/IConsensue.java
+++ src/main/java/org/bcia/javachain/consenter/consensus/IConsensue.java
@@ -26,98 +26,97 @@ import java.util.Map;
  * @company Dingxuan
  */
 public interface IConsensue {
-    public Chain HandleChain(ConsenterSupport consenterSupport, Common.Metadata metadata);
+     IChain handleChain(IConsenterSupport consenterSupport, Common.Metadata metadata) throws IllegalAccessException, InstantiationException;
 
 
-    public interface Chain {
-        public void Order(Common.Envelope env, long configSeq);
-        public void Configure(Common.Envelope config,long configSeq);
-
-        /**
-         * 未定义方法Errored() <-chan struct{}
-         */
-        public void Start();
-        public void Halt();
-
-    }
-    public interface ConsenterSupport {
-        /**
-         * crypto.LocalSigner
-         */
-        public interface LocalSigner {
-            public Common.SignatureHeader NewSignatureHeader();
-
-            public byte[] Sign(byte[] message);
-        }
-
-        /**
-         * msgprocessor.Processor 接口
-         */
-        public interface Processor {
-            public int ClassfiyMsg(Common.GroupHeader chdr);
-
-            public long ProcessNormalMsg(Common.Envelope env);
-
-            /**
-             * 返回env 和 configSeq ,之后转换为实体
-             */
-            public Object ProcessConfigUpdateMsg(Common.Envelope env);
-
-            /**
-             * 返回类型为(*cb.Envelope, uint64, error) 之后做作补充
-             */
-            public Object ProcessConfigMsg(Common.Envelope env);
-        }
-
-
-        public Receiver BlockCutter();
-        public Orderer SharedConfig();
-        public Common.Block CreateNextBlock(Common.Envelope[] messages);
-        public void  WriteBlock(Common.Block block,byte[] encodedMetadataValue);
-        public void WriteConfig(Common.Block block,byte[] encodedMetadataValue);
-        public long Sequence();
-        public String ChainID();
-        public long Height();
-
-
-
-    }
-
-
-
-
-    public interface Receiver {
-        public void Ordered(Common.Envelope[][] messagesBatches, boolean pending);
-
-        public Common.Envelope[] Cut();
-    }
-
-    public interface Orderer {
-        public String ConsensusType();
-
-        public Configuration.BatchSize BatchSize();
-
-        /**
-         * 缺失 time.Duration
-         */
-
-        public long MaxChannelsCount();
-
-        public String[] KafkaBrokers();
-
-         public Map<String,Org> Organizations();
-       public  OrdererCapabilities Capabilities();
-
-    }
-    public interface Org{
-        public String Name();
-        public String MSPID();
-    }
-
-    public interface  OrdererCapabilities{
-        public boolean SetChannelModPolicyDuringCreate();
-        public boolean Resubmission();
-        public void Supported();
-    }
+//    public interface Chain {
+//        public void order(Common.Envelope env, long configSeq);
+//        public void configure(Common.Envelope config,long configSeq);
+//
+//        /**
+//         * 未定义方法Errored() <-chan struct{}
+//         */
+//        public void start();
+//        public void halt();
+//
+//    }
+//    public interface IConsenterSupport {
+//        /**
+//         * crypto.LocalSigner
+//         */
+//        public interface LocalSigner {
+//            public Common.SignatureHeader newSignatureHeader();
+//
+//            public byte[] sign(byte[] message);
+//        }
+//
+//        /**
+//         * msgprocessor.Processor 接口
+//         */
+//        public interface processor {
+//            public int classfiyMsg(Common.GroupHeader chdr);
+//
+//            public long processNormalMsg(Common.Envelope env);
+//
+//            /**
+//             * 返回env 和 configSeq ,之后转换为实体
+//             */
+//            public Object processConfigUpdateMsg(Common.Envelope env);
+//
+//            /**
+//             * 返回类型为(*cb.Envelope, uint64, error) 之后做作补充
+//             */
+//            public Object processConfigMsg(Common.Envelope env);
+//        }
+//
+//
+//        public receiver blockCutter();
+//        public Orderer sharedConfig();
+//        public Common.Block createNextBlock(Common.Envelope[] messages);
+//        public void  writeBlock(Common.Block block,byte[] encodedMetadataValue);
+//        public void writeConfig(Common.Block block,byte[] encodedMetadataValue);
+//        public long sequence();
+//        public String chainID();
+//        public long height();
+//
+//
+//    }
+//
+//
+//
+//
+//    public interface receiver {
+//        public void ordered(Common.Envelope[][] messagesBatches, boolean pending);
+//
+//        public Common.Envelope[] Cut();
+//    }
+//
+//    public interface Orderer {
+//        public String ConsensusType();
+//
+//        public Configuration.BatchSize BatchSize();
+//
+//        /**
+//         * 缺失 time.Duration
+//         */
+//
+//        public long maxChannelsCount();
+//
+//        public String[] KafkaBrokers();
+//
+//         public Map<String,Org> Organizations();
+//       public  OrdererCapabilities Capabilities();
+//
+//    }
+//    public interface Org{
+//        public String name();
+//        public String mspid();
+//    }
+//
+//    public interface  OrdererCapabilities{
+//        public boolean setChannelModPolicyDuringCreate();
+//        public boolean resubmission();
+//        public void supported();
+//    }
 
 }
