4f59556fbea72a01a128347118205a0231c902e6
wanliangbing <wlb@dxct.org>
2018-03-12 14:56:26 +0800

    [BJC-33][BJC-34][BJC-35][BJC-36] 1、增加测试类

---
+++ src/test/java/org/bcia/javachain/core/ledger/leveldb/LevelDBFactoryTest.java
@@ -0,0 +1,93 @@
+package org.bcia.javachain.core.ledger.leveldb;
+
+import org.junit.Test;
+
+/**
+ * LevelDBFactoryTest测试类
+ *
+ * @author
+ * @date 2018/3/12
+ * @company Dingxuan
+ */
+public class LevelDBFactoryTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void getDB() {
+    }
+
+    @Test
+    public void getDB1() {
+    }
+
+    @Test
+    public void closeDB() {
+    }
+
+    @Test
+    public void closeWriteBatch() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void add1() {
+    }
+
+    @Test
+    public void add2() {
+    }
+
+    @Test
+    public void add3() {
+    }
+
+    @Test
+    public void add4() {
+    }
+
+    @Test
+    public void add5() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void delete1() {
+    }
+
+    @Test
+    public void delete2() {
+    }
+
+    @Test
+    public void delete3() {
+    }
+
+    @Test
+    public void delete4() {
+    }
+
+    @Test
+    public void delete5() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void get1() {
+    }
+
+    @Test
+    public void get2() {
+    }
+}
