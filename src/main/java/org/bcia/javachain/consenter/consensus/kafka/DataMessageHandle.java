d8a93bb7c1e8b8bb491775b264239ab5079ed373
yangdong <yangdong@shudun.com>
2018-03-19 16:09:09 +0800

    预定义数据类型转换方法

---
+++ src/main/java/org/bcia/javachain/consenter/consensus/kafka/DataMessageHandle.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright Dingxuan. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bcia.javachain.consenter.consensus.kafka;
+
+/**
+ * 类描述
+ *
+ * @author yangdong
+ * @date 2018/3/19
+ * @company Shudun
+ */
+
+public class DataMessageHandle {
+
+    //转换kafka连接消息
+    public void newConnectMessage(){
+
+    }
+
+    //转换Enqueue（普通）方法所用消息格式
+    public void newNormalMessage(){
+
+    }
+
+    //转换Enqueue（配置）方法所用消息格式
+    public void newConfigMessage(){
+
+    }
+
+    //转换数据切块方法
+    public void newTimeToCutMessage(){
+
+    }
+
+    //生产者消息转换
+    public void newProducerMessage(){
+
+    }
+}
