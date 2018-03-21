6c930ac7ab974b9d1186e72192d7327180d82e52
yangdong <yangdong@shudun.com>
2018-03-21 13:37:41 +0800

    [BJC-65]完成启动kafka消费者服务（未完成参数从文件中读取）

--- src/main/java/org/bcia/javachain/consenter/consensus/kafka/Chain.java
+++ src/main/java/org/bcia/javachain/consenter/consensus/kafka/Chain.java
@@ -20,6 +20,9 @@ import org.bcia.javachain.protos.common.Common;
 import org.bcia.javachain.protos.consenter.Kafka;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author yangdong
  * @Date: 2018/3/19
@@ -31,6 +34,8 @@ public class Chain implements IChain {
     private DataMessageHandle dataMessageHandle;
     @Autowired
     private KafkaProduce kafkaProduce;
+    @Autowired
+    private KafkaSimpleConsumer kafkaSimpleConsumer;
 
     @Override
     public void order(Common.Envelope env, long configSeq) {
@@ -44,7 +49,12 @@ public class Chain implements IChain {
 
     @Override
     public void start() {
-
+        //新启动一个线程，来处理消费消息
+        Thread t = new Thread(new Runnable(){
+            public void run(){
+                startThread();
+            }});
+        t.start();
     }
 
     //Halt释放被分配给这个Chain的资源
@@ -66,7 +76,10 @@ public class Chain implements IChain {
 
     }
    //实现kafka消费者，start调用该方法
-    public void processMessagesToBlocks(){
+    public void processMessagesToBlocks(byte[] message){
+
+    //解析数据，分类处理
+
 
     }
     //处理消息（重发消息，切块等）区分值三种（未知，配置，正常）的消息类型
@@ -92,6 +105,19 @@ public class Chain implements IChain {
     //1.创建生产者，消费者，分区消费者，父类消费者，2.调用消费者方法，调用方法
     //start方法调用该方法
     public void startThread(){
+        long maxReads = 300;
+        String topic = "test2";
+        int partitionID = 0;
+
+        KafkaTopicPartitionInfo topicPartitionInfo = new KafkaTopicPartitionInfo(topic, partitionID);
+        List<KafkaBrokerInfo> seeds = new ArrayList<KafkaBrokerInfo>();
+        seeds.add(new KafkaBrokerInfo("10.0.20.91", 9092));
+        seeds.add(new KafkaBrokerInfo("10.0.20.92", 9092));
+        try {
+            kafkaSimpleConsumer.run(maxReads, topicPartitionInfo, seeds);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -117,5 +143,9 @@ public class Chain implements IChain {
         enqueue(kafkaMessage);
     }
 
+    public static void main(String[] args) {
+        Chain cc=new Chain();
+        cc.start();
+    }
 
 }
