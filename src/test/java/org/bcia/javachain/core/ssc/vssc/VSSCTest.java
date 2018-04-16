f409427aab85938852bcb261c6dd52966f502483
snl <snl@dxct.org>
2018-04-16 16:50:15 +0800

    [BJC-45] 完成QSSC单元测试框架

---
+++ src/test/java/org/bcia/javachain/core/ssc/vssc/VSSCTest.java
@@ -0,0 +1,23 @@
+package org.bcia.javachain.core.ssc.vssc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * VSSC单元测试类
+ *
+ * @author sunianle
+ * @date 4/16/18
+ * @company Dingxuan
+ */
+public class VSSCTest {
+
+    @Test
+    public void init() {
+    }
+
+    @Test
+    public void invoke() {
+    }
+}
