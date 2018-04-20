6a0cd387e80e8f8ccb1e33d129373a038a400ed0
sunzongyu <szy@dxct.org>
2018-04-20 09:55:23 +0800

    BJC-34

--- src/main/java/org/bcia/javachain/core/ledger/kvledger/txmgmt/txmgr/lockbasedtxmgr/QueryHelper.java
+++ src/main/java/org/bcia/javachain/core/ledger/kvledger/txmgmt/txmgr/lockbasedtxmgr/QueryHelper.java
@@ -18,8 +18,13 @@ package org.bcia.javachain.core.ledger.kvledger.txmgmt.txmgr.lockbasedtxmgr;
 import org.bcia.javachain.common.exception.LedgerException;
 import org.bcia.javachain.common.ledger.ResultsIterator;
 import org.bcia.javachain.core.ledger.kvledger.txmgmt.rwsetutil.RWSetBuilder;
+import org.bcia.javachain.core.ledger.kvledger.txmgmt.statedb.VersionedValue;
+import org.bcia.javachain.core.ledger.kvledger.txmgmt.version.Height;
+import org.bcia.javachain.protos.ledger.rwset.kvrwset.KvRwset;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 类描述
@@ -29,13 +34,139 @@ import java.util.List;
  * @company Dingxuan
  */
 public class QueryHelper {
-    private LockBasedTxMgr txMgr;
+    private LockBasedTxManager txMgr;
     private RWSetBuilder rwSetBuilder;
     private List<ResultsItr> itrs;
     private boolean doneInvoked;
 
     public byte[] getState(String ns, String key) throws LedgerException{
         checkDone();
+        VersionedValue versionedValue = txMgr.getDb().getState(ns, key);
+        byte[] val = versionedValue.getValue();
+        Height ver = versionedValue.getVersion();
+        addToReadSet(ns, key, ver);
+        return val;
+    }
+
+    public List<byte[]> getStateMultipleKeys(String ns, String[] keys) throws LedgerException{
+        checkDone();
+        List<VersionedValue> versionedValues = txMgr.getDb().getStateMultipleKeys(ns, keys);
+        List<byte[]> values = new ArrayList<>();
+        for (int i = 0; i < versionedValues.size(); i++) {
+            byte[] val = versionedValues.get(i).getValue();
+            Height ver = versionedValues.get(i).getVersion();
+            addToReadSet(ns, keys[i], ver);
+            values.add(val);
+        }
+        return values;
+    }
+
+    public ResultsIterator getStateRangeScanIterator(String ns, String startKey, String endKey) throws LedgerException{
+        checkDone();
+        //TODO ledger config
+        ResultsItr itr = ResultsItr.newResultsItr(ns, startKey, endKey, txMgr.getDb(), rwSetBuilder, false, 50);
+        itrs.add(itr);
+        return itr;
+    }
+
+    public ResultsIterator executeQuery(String ns, String query) throws LedgerException {
+        checkDone();
+        ResultsIterator dbItr = txMgr.getDb().executeQuery(ns, query);
+        QueryResultsItr itr = new QueryResultsItr();
+        itr.setDbItr(dbItr);
+        itr.setRwSetBuilder(rwSetBuilder);
+        return itr;
+    }
+
+    public byte[] getPrivateData(String ns, String coll, String key) throws LedgerException {
+        checkDone();
+        VersionedValue versionedValue = txMgr.getDb().getPrivateData(ns, coll, key);
+        byte[] val = versionedValue.getValue();
+        Height ver = versionedValue.getVersion();
+        //TODO 获取keyhash
+        byte[] keyHash = "KeyHash".getBytes();
+        Height hashVersion = txMgr.getDb().getKeyHashVersion(ns, coll, keyHash);
+        if(!Height.areSame(ver, hashVersion)){
+            throw new LedgerException(String.format("Private data matching public hash version is not available.Pub version %s. Pvt version %s",
+                    ver, hashVersion));
+        }
+        addToHashedReadSet(ns, coll, key, ver);
+        return val;
+    }
+
+    public List<byte[]> getPrivateDataMultipleKeys(String ns, String coll, String[] keys) throws LedgerException {
+        checkDone();
+        List<VersionedValue> versionedValues = txMgr.getDb().getPrivateDataMultipleKeys(ns, coll, keys);
+        List<byte[]> values = new ArrayList<>();
+        for (int i = 0; i < versionedValues.size() ; i++) {
+            byte[] val = versionedValues.get(i).getValue();
+            Height ver = versionedValues.get(i).getVersion();
+            addToHashedReadSet(ns, coll, keys[i], ver);
+            values.add(val);
+        }
+        return values;
+    }
+
+    public ResultsIterator getPrivateDataRangeScanIterator(String ns, String coll, String startKey, String endKey) throws LedgerException {
+        checkDone();
+        ResultsIterator dbitr = txMgr.getDb().getPrivateDataRangeScanIterator(ns, coll, startKey, endKey);
+        PvtdataResultsItr itr = new PvtdataResultsItr();
+        itr.setNs(ns);
+        itr.setColl(coll);
+        itr.setDbItr(dbitr);
+        return itr;
+    }
+
+    public ResultsIterator executeQueryOnPrivateData(String ns, String coll, String query) throws LedgerException {
+        checkDone();
+        ResultsIterator dbitr = txMgr.getDb().executeQueryOnPrivateData(ns, coll, query);
+        PvtdataResultsItr itr = new PvtdataResultsItr();
+        itr.setNs(ns);
+        itr.setColl(coll);
+        itr.setDbItr(dbitr);
+        return itr;
+    }
+
+    public void done(){
+        if(doneInvoked){
+            return;
+        }
+        for(ResultsItr itr : itrs){
+            if (rwSetBuilder != null){
+                Map.Entry<List<KvRwset.KVRead>, KvRwset.QueryReadsMerkleSummary> entry = itr.getRangeQueryResultsHelper().done();
+                if(entry.getKey() != null){
+                    KvRwset.RangeQueryInfo rqi = itr.getRangeQueryInfo();
+                    KvRwset.QueryReads.Builder builder = rqi.getRawReads().toBuilder();
+                    builder = setKvReads(builder, entry.getKey());
+                    rqi.toBuilder().setRawReads(builder);
+                    itr.setRangeQueryInfo(rqi);
+                }
+                if(entry.getValue() != null){
+                    itr.setRangeQueryInfo(itr.getRangeQueryInfo().toBuilder().setReadsMerkleHashes(entry.getValue()).build());
+                }
+                rwSetBuilder.addToRangeQuerySet(itr.getNs(), itr.getRangeQueryInfo());
+            }
+        }
+    }
+
+    //grpc set各种list貌似只能一个一个添加:( ...
+    private KvRwset.QueryReads.Builder setKvReads(KvRwset.QueryReads.Builder builder, List<KvRwset.KVRead> list){
+        for (int i = 0; i < list.size(); i++) {
+            builder.setKvReads(i, list.get(i));
+        }
+        return builder;
+    }
+
+    private void addToReadSet(String ns, String key, Height ver){
+        if(rwSetBuilder != null){
+            rwSetBuilder.addToReadSet(ns, key, ver);
+        }
+    }
+
+    private void addToHashedReadSet(String ns, String coll, String key, Height ver){
+        if(rwSetBuilder != null){
+            rwSetBuilder.addToHashedReadSet(ns, coll, key, ver);
+        }
     }
 
     private void checkDone() throws LedgerException{
@@ -44,11 +175,11 @@ public class QueryHelper {
         }
     }
 
-    public LockBasedTxMgr getTxMgr() {
+    public LockBasedTxManager getTxMgr() {
         return txMgr;
     }
 
-    public void setTxMgr(LockBasedTxMgr txMgr) {
+    public void setTxMgr(LockBasedTxManager txMgr) {
         this.txMgr = txMgr;
     }
 
