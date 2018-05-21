b2d225790a36f9f77fb30ac8c655b8370e19186f
wanliangbing <wlb@dxct.org>
2018-05-21 12:59:43 +0800

    [BJC-30] 还原到de4c6d275141f1b91f3ca2fd10819a3122c09fdd版本

---
+++ src/test/java/org/bcia/javachain/core/ledger/leveldb/LevelDBUtilTest.java
@@ -0,0 +1,49 @@
+package org.bcia.javachain.core.ledger.leveldb;
+
+import org.junit.Test;
+
+/**
+ * 类描述
+ *
+ * @author wanliangbing
+ * @date 2018/4/27
+ * @company Dingxuan
+ */
+public class LevelDBUtilTest {
+
+  @Test
+  public void getDB() {}
+
+  @Test
+  public void removeDb() {}
+
+  @Test
+  public void closeDB() {}
+
+  @Test
+  public void closeSnapshot() {}
+
+  @Test
+  public void closeWriteBatch() {}
+
+  @Test
+  public void add() {}
+
+  @Test
+  public void add1() {}
+
+  @Test
+  public void delete() {}
+
+  @Test
+  public void delete1() {}
+
+  @Test
+  public void get() {}
+
+  @Test
+  public void getIterator() {}
+
+  @Test
+  public void getLastKey() {}
+}
