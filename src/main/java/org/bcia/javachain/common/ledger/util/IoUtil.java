fcd23b3a3359ca7b293bbc0c44de83f1b4f44a24
sunzongyu <szy@dxct.org>
2018-05-22 17:34:52 +0800

    BJC-34 智能合约打包

--- src/main/java/org/bcia/javachain/common/ledger/util/IoUtil.java
+++ src/main/java/org/bcia/javachain/common/ledger/util/IoUtil.java
@@ -157,232 +157,190 @@ public class IoUtil {
             throw new JavaChainException(e);
         }
     }
-
     /**
-     * 将文件打包为.tar.gz
-     * @param sourceFile 源文件
-     * @param outPutPath 输出文件夹
-     * @param fileName 输出文件名
-     * @throws JavaChainException
+     * 获取文件价下所有文件相对文件夹路径
+     * @param path 文件夹路径
+     * @return
      */
-    public static void tarGzWriter(String sourceFile, String outPutPath, String fileName) throws JavaChainException{
-        List<File> l = new ArrayList<>();
-        File source = new File(sourceFile);
-        if(source.isDirectory()){
-            l = listFiles(source);
-        } else {
-            l.add(source);
+    public static Map<String, File> getFileRelativePath(String path){
+        File file = new File(path);
+        if(!file.exists()){
+            // TODO: 5/22/18 need log
+            return null;
         }
-        tarWriter(l, outPutPath, fileName);
-    }
-
-    /**
-     * 将文件中所有文件加入list
-     * @param sourceFile 源文件夹
-     * @return 包含所有文件的list
-     */
-    private static List<File> listFiles(File sourceFile){
-        Queue<File> q = new LinkedList<>();
-        List<File> l = new ArrayList<>();
-        q.add(sourceFile);
-        while(q.size() > 0){
-            File file = q.poll();
-            if (file.isDirectory()) {
-                q.addAll(Arrays.asList(file.listFiles()));
-            } else {
-                l.add(file);
+        if(!file.isDirectory()){
+            // TODO: 5/22/18 need log
+            return null;
+        }
+        Map<String, File> result = new HashMap<>();
+        Queue<File> queue = new LinkedList<>();
+        queue.add(file);
+        while(queue.size() > 0){
+            File tmpFile = queue.poll();
+            if(tmpFile.isDirectory()){
+                File[] tmpFiles = tmpFile.listFiles();
+                if (tmpFiles != null) {
+                    queue.addAll(Arrays.asList(tmpFiles));
+                }
+            } else if(tmpFile.isFile()) {
+                result.put(tmpFile.getAbsolutePath().substring(file.getAbsolutePath().length() + 1), tmpFile);
             }
         }
-        return l;
+        return result;
     }
 
     /**
-     * 将所有文件打tar包
-     * @param files 需要打包的文件
-     * @param outPutPath 输出文件路径
-     * @param fileName 输出文件名
-     * @return tar文件
+     * 将文件制作tar包
+     * @param files 文件集合
+     *              key:文件路径
+     *              value:文件对象
+     * @param cache 缓冲区大小
+     * @return tar 字节流
      */
-    public static File tarWriter(List<File> files, String outPutPath, String fileName){
-        File outPutFile = null;
+    public static byte[] tarWriter(Map<String, File> files, int cache) throws JavaChainException{
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        TarArchiveOutputStream taos = new TarArchiveOutputStream(baos);
         FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        FileOutputStream fos = null;
-        GZIPOutputStream gzp = null;
-        File tar = new File(outPutPath + File.separator + "temp.tar");
+        TarArchiveEntry tae = null;
         try {
-            fis = new FileInputStream(gzipWriter(files, tar));
-            bis = new BufferedInputStream(fis, BUFFER);
-            outPutFile = new File(outPutPath + "/" + checkFileName(fileName) + ".tar.gz");
-            fos = new FileOutputStream(outPutFile);
-            gzp = new GZIPOutputStream(fos);
-            int count;
-            byte[] data = new byte[BUFFER];
-            while ((count = bis.read(data, 0, BUFFER)) != -1) {
-                gzp.write(data, 0, count);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            try {
-                if(gzp != null){
-                    gzp.finish();
-                    gzp.flush();
-                    gzp.close();
-                }
-                if(fos != null){
-                    fos.close();
-                }
-                if(bis != null){
-                    bis.close();
-                }
-                if(fis != null){
-                    fis.close();
-                }
-                if(tar.exists()){
-                    tar.delete();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return outPutFile;
-    }
-
-    /**
-     * 压缩文件
-     * @param files 所需压缩的文件
-     * @param target 压缩后目标文件
-     * @return 目标文件对象
-     */
-    public static File gzipWriter(List<File> files, File target){
-        FileOutputStream fos = null;
-        BufferedOutputStream bos = null;
-        TarArchiveOutputStream taos = null;
-        FileInputStream fis = null;
-        TarArchiveEntry entry = null;
-        try {
-            fos  = new FileOutputStream(target);
-            bos = new BufferedOutputStream(fos, BUFFER);
-            taos = new TarArchiveOutputStream(bos);
-            //解决文件名过长问题
-            taos.setLongFileMode(TarArchiveOutputStream.LONGFILE_GNU);
-            int i = 0;
-            for(File file : files){
-                entry = new TarArchiveEntry(file);
-                entry.setName(file.getName());
-                i++;
-                taos.putArchiveEntry(entry);
+            for (Map.Entry<String, File> entry : files.entrySet()) {
+                String fileName = entry.getKey();
+                File file = entry.getValue();
                 fis = new FileInputStream(file);
-                IOUtils.copy(fis, taos);
+                tae = new TarArchiveEntry(file);
+                tae.setName(fileName);
+                taos.putArchiveEntry(tae);
+                int num = 0;
+                byte[] buff = new byte[cache];
+                while((num = fis.read(buff)) != -1){
+                    taos.write(buff, 0, num);
+                }
                 taos.closeArchiveEntry();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            try {
-                if(fis != null){
-                    fis.close();
-                }
-                if(taos != null){
-                    taos.finish();
-                    taos.flush();
-                    taos.close();
-                }
-                if(bos != null){
-                    bos.flush();
-                    bos.close();
-                }
-                if(fos != null){
-                    fos.flush();
-                    fos.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return target;
-    }
-
-    /**
-     * 读取.tar.gz文件
-     * @param sourceFile 源文件
-     * @param targetDir 目标目录
-     * @throws JavaChainException
-     */
-    public static void tarGzReader(String sourceFile, String targetDir) throws JavaChainException{
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        GZIPInputStream gzis = null;
-        TarArchiveInputStream tais = null;
-        OutputStream os = null;
-        try {
-            File tarGzFile = new File(sourceFile);
-            String tarGzFileName = tarGzFile.getName();
-            //目标文件扩展名错误
-            checkFileName(tarGzFileName);
-            //目标目录不存在时创建新目录
-            File targetDirFile = new File(targetDir);
-            if(!targetDirFile.exists()){
-                if(!targetDirFile.mkdir()){
-                    throw new JavaChainException("Fail to create file " + targetDir);
-                }
-                if(!targetDirFile.setWritable(true, false)){
-                    throw new JavaChainException("Fail to set file writable " + targetDir);
-                }
-            }
-            fis = new FileInputStream(tarGzFile);
-            bis = new BufferedInputStream(fis);
-            gzis = new GZIPInputStream(bis);
-            tais = new TarArchiveInputStream(gzis);
-            TarArchiveEntry tae = null;
-            //循环读取文件并写入目标文件
-            while ((tae = tais.getNextTarEntry()) != null) {
-                File tmpFile = new File(targetDir + File.separator + tae.getName());
-                os = new FileOutputStream(tmpFile);
-                int length = 0;
-                byte[] b = new byte[BUFFER];
-                while ((length = tais.read(b)) != -1) {
-                    os.write(b, 0, length);
-                }
-            }
         } catch (IOException e) {
+            // TODO: 5/22/18 log needed
             throw new JavaChainException(e);
         } finally {
-
             try {
-                if (fis != null) {
-                    fis.close();
-                }
-                if (bis != null) {
-                    bis.close();
-                }
-                if (gzis != null) {
-                    gzis.close();
-                }
-                if (tais != null) {
-                    tais.close();
-                }
-                if (os != null) {
-                    os.close();
-                }
+                baos.close();
+                taos.close();
+                fis.close();
             } catch (IOException e) {
+                // TODO: 5/22/18 log needed
                 throw new JavaChainException(e);
             }
         }
+        return baos.toByteArray();
     }
 
-    private static String checkFileName(String fileName) throws JavaChainException{
-        String fileNameEnding = ".tar.gz";
-        if(fileName.length() > fileNameEnding.length() && fileName.substring(fileName.length() - fileNameEnding.length()).equals(fileNameEnding)){
-            return fileName.substring(0, fileName.length() - fileNameEnding.length());
-        } else {
-            throw new JavaChainException("Wrong file name " + fileName);
+    /**
+     * 使用gzip压缩文件
+     * @param fileBytes 将要压缩的文件流
+     * @return gzip压缩的文件流
+     */
+    public static byte[] gzipWriter(byte[] fileBytes) throws JavaChainException{
+        ByteArrayOutputStream baos = null;
+        GZIPOutputStream gzos = null;
+        try {
+            baos = new ByteArrayOutputStream();
+            gzos = new GZIPOutputStream(baos);
+            gzos.write(fileBytes, 0, fileBytes.length);
+            gzos.finish();
+            gzos.flush();
+        } catch (IOException e) {
+            // TODO: 5/22/18 log needed
+            throw new JavaChainException(e);
+        } finally {
+            try {
+                baos.close();
+                gzos.close();
+            } catch (IOException e) {
+                // TODO: 5/22/18 log needed
+                throw new JavaChainException(e);
+            }
         }
+        return baos.toByteArray();
+    }
+
+    /**
+     * 解压缩tar包
+     * @param tarBytes tar包字节流
+     * @param cache 缓冲大小
+     * @return 文件集合
+     *          key:文件相对路径
+     *          value:文件流
+     */
+    public static Map<String, byte[]> tarReader(byte[] tarBytes, int cache) throws JavaChainException{
+        Map<String, byte[]> result = new HashMap<>();
+        ByteArrayOutputStream baos = null;
+        ByteArrayInputStream bais = null;
+        TarArchiveInputStream tais = null;
+        try {
+            baos = new ByteArrayOutputStream();
+            bais = new ByteArrayInputStream(tarBytes);
+            tais = new TarArchiveInputStream(bais);
+            TarArchiveEntry tae = null;
+            while((tae = tais.getNextTarEntry()) != null){
+                int len = 0;
+                byte[] buff = new byte[cache];
+                while((len = tais.read(buff)) != -1){
+                    baos.write(buff, 0, len);
+                }
+                result.put(tae.getName(), baos.toByteArray());
+            }
+        } catch (IOException e) {
+            // TODO: 5/22/18 log needed
+            throw new JavaChainException(e);
+        } finally {
+            try {
+                baos.close();
+                bais.close();
+                tais.close();
+            } catch (IOException e) {
+                // TODO: 5/22/18 log needed
+                throw new JavaChainException(e);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 使用gzip解压文件
+     * @param fileBytes 文件流
+     * @param cache 缓冲大小
+     * @return 解压后文件流
+     */
+    public static byte[] gzipReader(byte[] fileBytes, int cache) throws JavaChainException{
+        ByteArrayOutputStream baos = null;
+        ByteArrayInputStream bais = null;
+        GZIPInputStream gzis = null;
+        try {
+            bais = new ByteArrayInputStream(fileBytes);
+            gzis = new GZIPInputStream(bais);
+            baos = new ByteArrayOutputStream();
+            byte[] buff = new byte[cache];
+            int num = 0;
+            while((num = gzis.read(buff)) > 0){
+                baos.write(buff, 0, num);
+            }
+        } catch (IOException e) {
+            // TODO: 5/22/18 log needed
+            throw new JavaChainException(e);
+        } finally {
+            try {
+                bais.close();
+                gzis.close();
+                baos.close();
+            } catch (IOException e) {
+                // TODO: 5/22/18 log needed
+                throw new JavaChainException(e);
+            }
+        }
+        return baos.toByteArray();
     }
 
     public static void main(String[] args) throws JavaChainException {
 //        tarGzWriter("/home/bcia/123/4-11", "/home/bcia/123", "4-1.tar.gz");
-        tarGzReader("/home/bcia/123/4-1.tar.gz", "/home/bcia/4-111");
+//        tarGzReader("/home/bcia/123/4-1.tar.gz", "/home/bcia/4-111");
     }
 }
