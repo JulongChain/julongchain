ad1ee92733a1321be5f175f8b1fdff5675129cd0
aisino-yuanjun <463324047@qq.com>
2018-06-04 14:43:14 +0800

    Merge branch 'develop' of ssh://gitlab.bcia.net.cn:13622/bcia/javachain into develop
    # Conflicts:
    #	src/main/java/org/bcia/javachain/common/policycheck/IPolicyChecker.java
    #	src/main/java/org/bcia/javachain/common/policycheck/PolicyChecker.java
    #	src/main/java/org/bcia/javachain/common/policycheck/cauthdsl/CAuthDsl.java
    #	src/test/java/org/bcia/javachain/common/policycheck/PolicyCheckerTest.java

--- src/test/java/org/bcia/javachain/common/policycheck/CAuthDslTest.java
+++ src/test/java/org/bcia/javachain/common/policycheck/CAuthDslTest.java
@@ -19,7 +19,7 @@ package org.bcia.javachain.common.policycheck;
 import org.bcia.javachain.common.exception.PolicyException;
 import org.bcia.javachain.common.policycheck.cauthdsl.CAuthDsl;
 import org.bcia.javachain.common.util.proto.SignedData;
-import org.bcia.javachain.core.ssc.essc.MockMspManager;
+
 import org.bcia.javachain.msp.IIdentityDeserializer;
 import org.bcia.javachain.msp.mgmt.MspManager;
 import org.junit.After;
@@ -71,6 +71,6 @@ public class CAuthDslTest {
         sds.add(sd2);
         sds.add(sd3);
         MspManager deserializer = new MspManager();//mock(MspManager.class);
-        //CAuthDsl.deduplicate(sds,deserializer);
+
     }
 }
