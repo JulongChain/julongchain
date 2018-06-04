0d63a3e5e8ad2bca053bf1b5240b53ff34f9586c
wanliangbing <wlb@dxct.org>
2018-06-04 12:07:18 +0800

    [BJC-38] 调试实例化链码

--- src/test/java/org/bcia/javachain/common/policycheck/CAuthDslTest.java
+++ src/test/java/org/bcia/javachain/common/policycheck/CAuthDslTest.java
@@ -17,10 +17,7 @@
 package org.bcia.javachain.common.policycheck;
 
 import org.bcia.javachain.common.exception.PolicyException;
-import org.bcia.javachain.common.policycheck.cauthdsl.CAuthDsl;
 import org.bcia.javachain.common.util.proto.SignedData;
-import org.bcia.javachain.core.ssc.essc.MockMspManager;
-import org.bcia.javachain.msp.IIdentityDeserializer;
 import org.bcia.javachain.msp.mgmt.MspManager;
 import org.junit.After;
 import org.junit.Before;
@@ -28,8 +25,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.mockito.Mockito.mock;
 
 /**
  * 类描述
