8d6bbf910895a53d06bff1df57081b025bcdf733
wanliangbing <wlb@dxct.org>
2018-06-12 16:35:21 +0800

    [BJC-141] 修改为抛出GossipException

--- src/test/java/org/bcia/julongchain/gossip/GossipServiceUtilTest.java
+++ src/test/java/org/bcia/julongchain/gossip/GossipServiceUtilTest.java
@@ -1,10 +1,11 @@
 package org.bcia.julongchain.gossip;
 
 import org.apache.gossip.GossipService;
+import org.apache.gossip.GossipSettings;
+import org.bcia.julongchain.common.exception.GossipException;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.net.UnknownHostException;
 import java.util.UUID;
 
 /**
@@ -18,18 +19,14 @@ public class GossipServiceUtilTest {
 
   @Test
   /** 测试启动gossip服务 */
-  public void newGossipService() {
+  public void newGossipService() throws GossipException{
     String id = UUID.randomUUID().toString();
     String group = "group";
     String address = "localhost:7052";
 
-    try {
-      GossipServiceUtil.newGossipService(id, group, address).start();
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+    GossipServiceUtil.newGossipService(id, group, address).start();
+
+    GossipSettings s = new GossipSettings();
 
     GossipService gossipService = GossipServiceUtil.idAndGossipServiceMap.get(id);
     Assert.assertNotNull(gossipService);
@@ -42,25 +39,19 @@ public class GossipServiceUtilTest {
 
   @Test
   /** 测试上传数据 */
-  public void addData() {
+  public void addData() throws GossipException {
     String id = UUID.randomUUID().toString();
     String address = "localhost:7052";
     String group = "group";
-    try {
-      GossipService gossipService = GossipServiceUtil.newGossipService(id, group, address);
-      gossipService.start();
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+    GossipService gossipService = GossipServiceUtil.newGossipService(id, group, address);
+    gossipService.start();
     String data = "hello";
     GossipServiceUtil.addData(address, 1l, data);
   }
 
   @Test
   /** 测试上传数据，另一个节点读取数据 */
-  public void getData() {
+  public void getData() throws GossipException {
 
     String id1 = UUID.randomUUID().toString();
     String id2 = UUID.randomUUID().toString();
@@ -69,19 +60,13 @@ public class GossipServiceUtilTest {
     String address_readData = "localhost:7053";
     String group = "group111";
 
-    try {
-      GossipService gossipService_add =
-          GossipServiceUtil.newGossipService(id1, group, address_addData);
-      GossipService gossipService_read =
-          GossipServiceUtil.newGossipService(id2, group, address_readData);
+    GossipService gossipService_add =
+        GossipServiceUtil.newGossipService(id1, group, address_addData);
+    GossipService gossipService_read =
+        GossipServiceUtil.newGossipService(id2, group, address_readData);
 
-      gossipService_add.start();
-      gossipService_read.start();
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+    gossipService_add.start();
+    gossipService_read.start();
 
     String data = "hello gossip";
 
@@ -101,23 +86,19 @@ public class GossipServiceUtilTest {
 
   @Test
   /** 测试上传数据，另一个节点读取数据 */
-  public void getDataFromRemote() {
+  public void getDataFromRemote() throws GossipException {
 
     String id2 = UUID.randomUUID().toString();
 
     String address_readData = "192.168.1.50:7053";
     String group = "group";
 
-    try {
-      GossipService gossipService =
-          GossipServiceUtil.newGossipService(
-              id2, group, address_readData, UUID.randomUUID().toString(), "192.168.1.110:7052");
-      gossipService.start();
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+    GossipService gossipService =
+        GossipServiceUtil.newGossipService(
+            id2, group, address_readData, UUID.randomUUID().toString(), "192.168.1.110:7052");
+    gossipService.start();
+
+    GossipSettings s = new GossipSettings();
 
     try {
       Thread.sleep(2000);
