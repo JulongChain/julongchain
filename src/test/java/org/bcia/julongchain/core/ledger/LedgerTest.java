af5c7f92dc49324cb351b0bc97b06c61b60290af
sunzongyu <szy@dxct.org>
2018-06-15 09:13:59 +0800

    [BJC-113] 补充测试用例

--- src/test/java/org/bcia/julongchain/core/ledger/LedgerTest.java
+++ src/test/java/org/bcia/julongchain/core/ledger/LedgerTest.java
@@ -53,9 +53,9 @@ public class LedgerTest {
     @Test
     public void getKVFromLevelDB() throws Throwable {
 //        LevelDBProvider provider = new LevelDBProvider(LedgerConfig.getIndexPath());
-//        LevelDBProvider provider = new LevelDBProvider(LedgerConfig.getStateLevelDBPath());
+        LevelDBProvider provider = new LevelDBProvider(LedgerConfig.getStateLevelDBPath());
 //        LevelDBProvider provider = new LevelDBProvider(LedgerConfig.getHistoryLevelDBPath());
-        LevelDBProvider provider = new LevelDBProvider(LedgerConfig.getLedgerProviderPath());
+//        LevelDBProvider provider = new LevelDBProvider(LedgerConfig.getLedgerProviderPath());
 //        LevelDBProvider provider = new LevelDBProvider(LedgerConfig.getPvtDataStorePath());
 //        for (int i = 0; i < 100; i++) {
 //            Height height = new Height();
@@ -64,8 +64,6 @@ public class LedgerTest {
 //            byte[] value = height.toBytes();
 //            provider.put(ArrayUtils.addAll(ArrayUtils.addAll(("ns" + i).getBytes(), COMPOSITE_KEY_SEP), ("key" + i).getBytes()), value, true);
 //        }
-	    provider.put("key".getBytes(), "value".getBytes(), true);
-	    provider.delete("key".getBytes(), true);
         Iterator<Map.Entry<byte[], byte[]>> itr =  provider.getIterator(null);
         while(itr.hasNext()){
             Map.Entry<byte[], byte[]> entry = itr.next();
@@ -75,7 +73,7 @@ public class LedgerTest {
 //            soutBytes(entry.getKey());
             System.out.println(new String(entry.getKey()));
             System.out.println(new String(entry.getValue()));
-	        System.out.println(entry.getValue().length == 0);
+//	        System.out.println(entry.getValue().length == 0);
 //            soutBytes(entry.getValue());
 //            soutBytes(entry.getKey());
 //            soutBytes(entry.getValue());
