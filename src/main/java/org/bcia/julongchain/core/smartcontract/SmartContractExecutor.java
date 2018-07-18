4215b9688efd38364a336e062f06bc61de0f96be
wanliangbing <wlb@dxct.org>
2018-07-18 11:25:21 +0800

    [BJC-141] update docker image name

--- src/main/java/org/bcia/julongchain/core/smartcontract/SmartContractExecutor.java
+++ src/main/java/org/bcia/julongchain/core/smartcontract/SmartContractExecutor.java
@@ -106,6 +106,7 @@ public class SmartContractExecutor {
                 ProposalResponsePackage.Response response = null;
                 try {
                     response = ProposalResponsePackage.Response.parseFrom(responseMessage.getPayload());
+                    log.info(response.getMessage());
                 } catch (InvalidProtocolBufferException e) {
                     log.error(e.getMessage(), e);
                     throw new SmartContractException("Wrong Response");
