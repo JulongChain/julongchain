91f2f5b5c2b8cbfd982b254654917f505bb3040a
wanliangbing <wlb@dxct.org>
2018-07-24 15:20:35 +0800

    [BJC-141] add comments

--- src/main/java/org/bcia/julongchain/node/gossip/MspMessageCryptoService.java
+++ src/main/java/org/bcia/julongchain/node/gossip/MspMessageCryptoService.java
@@ -1,8 +1,25 @@
+/**
+ * Copyright Dingxuan. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.bcia.julongchain.node.gossip;
 
 import org.bcia.julongchain.common.exception.GossipException;
+import org.bcia.julongchain.common.localmsp.ILocalSigner;
 import org.bcia.julongchain.common.log.JavaChainLog;
 import org.bcia.julongchain.common.log.JavaChainLogFactory;
+import org.bcia.julongchain.common.policycheck.policies.IGroupPolicyManagerGetter;
 import org.bcia.julongchain.gossip.api.IMessageCryptoService;
 
 import java.sql.Timestamp;
@@ -10,6 +27,8 @@ import java.sql.Timestamp;
 public class MspMessageCryptoService implements IMessageCryptoService{
 
     private static final JavaChainLog log = JavaChainLogFactory.getLog(MspMessageCryptoService.class);
+    private IGroupPolicyManagerGetter channelPolicyManagerGetter;
+    private ILocalSigner localSigner;
 
     @Override
     public byte[] getPKIidOFCert(byte[] peerIdentity) {
